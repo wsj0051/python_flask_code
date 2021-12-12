f4183961d6d5f766a87a16060cd5b813b8819cc0
wsj0051 <wsj0051@163.com>
2021-12-12 18:58:54 +0800

    udp tcp

--- src/ senior/udp_test.py
+++ src/ senior/udp_test.py
@@ -4,8 +4,11 @@ import threading
 
 def recv_msg(udp_socket):
     while True:
-        recv_data = udp_socket.recvfrom(1024)
-        print(recv_data)
+        recv_msg = udp_socket.recvfrom(1024)
+        recv_ip = recv_msg[1]
+        recv_msg = recv_msg[0].decode("utf-8")
+        # 3. 显示接收到的数据
+        print(">>>%s:%s" % (str(recv_ip), recv_msg))
 
 
 def send_msg(udp_socket, dest_ip, dest_port):
@@ -16,11 +19,13 @@ def send_msg(udp_socket, dest_ip, dest_port):
 
 def main():
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp_socket.bind(("", 7890))
+    udp_socket.bind(("", 7788))
     dest_ip = input("请输入对方的ip:")
     dest_port = int(input("请输入对方端口："))
     t1 = threading.Thread(target=send_msg, args=(udp_socket, dest_ip, dest_port))
+    t1.start()
     t2 = threading.Thread(target=recv_msg, args=(udp_socket,))
+    t2.start()
 
 
 if __name__ == '__main__':
